cb7251e88852f07009cba24ef7abe0271a70b53c
Brayan Bautista <brayan.bautista@ibit.pe>
2024-08-01 20:50:01 -0500

    Avances app gastos

--- composeApp/src/commonTest/kotlin/ExpenseRepoTest.kt
+++ composeApp/src/commonTest/kotlin/ExpenseRepoTest.kt
@@ -1,10 +1,23 @@
 import data.ExpenseManager
 import data.ExpenseRepoImpl
+import model.Expense
+import kotlin.test.Test
+import kotlin.test.assertTrue
 
 class ExpenseRepoTest {
 
     private val expenseManager = ExpenseManager
     private val expenseRepo = ExpenseRepoImpl(expenseManager)
 
+    @Test
+    fun expense_list_is_not_empty() {
+        //GIVEN
+        val expenseList = mutableListOf<Expense>()
 
+        //WHEN
+        expenseList.addAll(expenseRepo.getAllExpenses())
+
+        //THEN
+        assertTrue(expenseList.isNotEmpty())
+    }
 }
